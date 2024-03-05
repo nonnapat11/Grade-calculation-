7a555f4bf42bf64733602a3fce25d19ef305e704
DL <sanidnoknonnapat@gmail.com>
2024-03-05 23:55:21 +0700

    edit code

--- function.py
+++ function.py
@@ -1,30 +1,26 @@
-def score():
-    accumulated = float(input('ใส่คะแนนเก็บ: '))
-    mid_score = float(input('ใส่คะแนนกลางภาค: '))
-    last_score = float(input('ใส่คะแนนปลายภาค: '))
-    self_score = float(input('ใส่คะแนนจิตพิสัย: '))
-
-def calculation(accumulated,mid_score,last_score,self_score):
+def score(accumulated,mid_score,last_score,self_score):
+    
     total_score = accumulated+mid_score+last_score+self_score
+    print()
+    print(f'คะแนน: {total_score}')
+
     if total_score > 100:
         print('กรุณาลองใหม่อีกครั้ง')
     elif total_score >= 80 and total_score <= 100:
-        print('A')
+        print('เกรด: A')
     elif total_score >= 75 and total_score <= 79:
-        print('B+')
+        print('เกรด: B+')
     elif total_score >= 70 and total_score <= 74:
-        print('B')
+        print('เกรด: B')
     elif total_score >= 65 and total_score <= 69:
-        print('C+')
+        print('เกรด: C+')
     elif total_score >= 60 and total_score <= 64:
-        print('C')
+        print('เกรด: C')
     elif total_score >= 55 and total_score <= 59:
-        print('D+')
+        print('เกรด: D+')
     elif total_score >= 50 and total_score <= 54:
-        print('D')
+        print('เกรด: D')
     elif total_score >= 0 and total_score <= 49:
-        print('F')
+        print('เกรด: F')
     else:
         print('กรุณาลองใหม่อีกครั้ง')
-
-    print(total_score)
--- grade.py
+++ grade.py
@@ -1,5 +1,20 @@
 from function import score
+import os
+import time
 
-score()
+try:
+    time.sleep(0.3)
+    accumulated = float(input('ใส่คะแนนเก็บ: '))
+    time.sleep(0.3)
+    mid_score = float(input('ใส่คะแนนกลางภาค: '))
+    time.sleep(0.3)
+    last_score = float(input('ใส่คะแนนปลายภาค: '))
+    time.sleep(0.3)
+    self_score = float(input('ใส่คะแนนจิตพิสัย: '))
+    time.sleep(0.3)
+except:
+    print('กรุณาลองใหม่อีกครั้ง และกรอกเป็นตัวเลขเท่านั้น!!!')
 
-develop
+os.system('cls')
+time.sleep(0.5)
+score(accumulated,mid_score,last_score,self_score)
